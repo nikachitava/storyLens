8d6f592421b8890883da95a8bec15b64eab8349f
nikachitava <chitava18.official@gmail.com>
2024-08-04 01:19:00 +0400

    login form validation with react-hook-form

--- src/components/LoginFormModal.tsx
+++ src/components/LoginFormModal.tsx
@@ -1,32 +1,85 @@
 import { Button, Label, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type LoginFormInputs = {
+	email: string;
+	password: string;
+};
 
 const LoginFormModal = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<LoginFormInputs>();
+
+	const onSubmit: SubmitHandler<LoginFormInputs> = (data) =>
+		console.log(data);
+
 	return (
-		<form className="flex max-w-lg flex-col gap-4 p-8 bg-lightblack lg:w-[500px]">
+		<form
+			className="flex max-w-lg flex-col gap-4 p-8 bg-lightblack lg:w-[500px]"
+			onSubmit={handleSubmit(onSubmit)}
+		>
 			<div>
 				<div className="mb-2 block">
-					<Label htmlFor="email2" value="Your email" />
+					<Label value="Your email" />
 				</div>
 				<TextInput
-					id="email2"
-					type="email"
-					placeholder=""
-					required
 					shadow
+					color={errors.email ? "failure" : "gray"}
+					{...register("email", {
+						required: {
+							value: true,
+							message: "Email field is required",
+						},
+						pattern: {
+							value: /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/,
+							message: "Enter correct email address",
+						},
+					})}
+					helperText={
+						errors.email && (
+							<>
+								<span className="font-medium">
+									{errors.email.message}
+								</span>
+							</>
+						)
+					}
 				/>
 			</div>
 			<div>
 				<div className="mb-2 block">
 					<Label value="Your password" />
 				</div>
-				<TextInput type="password" required shadow />
+				<TextInput
+					type="password"
+					color={errors.password ? "failure" : "gray"}
+					shadow
+					{...register("password", {
+						required: {
+							value: true,
+							message: "Password field is required",
+						},
+					})}
+					helperText={
+						errors.password && (
+							<>
+								<span className="font-medium">
+									{errors.password.message}
+								</span>
+							</>
+						)
+					}
+				/>
 			</div>
 
 			<div className="flex items-center gap-2">
 				<Label htmlFor="agree" className="flex">
 					<Link
-						to={"#"}
+						to={""}
 						className="text-cyan-600 hover:underline dark:text-cyan-500"
 					>
 						Forgot password?
